b19e50b523a703f4915f2e1388babac28b6b3422
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-06-28 00:05:23 +0900

    init

---
+++ src/main/java/com/server/bbo_gak/BboGakApplication.java
@@ -0,0 +1,13 @@
+package com.server.bbo_gak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BboGakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BboGakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/bbo_gak/BboGakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.bbo_gak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BboGakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
