912a19ee348f0762226687e55605100f6dc87e59
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-06-30 16:59:43 +0900

    [fix][#000] security config 11 -> 17 마이그레이션

--- src/main/java/com/server/bbo_gak/global/config/SecurityConfig.java
+++ src/main/java/com/server/bbo_gak/global/config/SecurityConfig.java
@@ -1,21 +1,20 @@
 package com.server.bbo_gak.global.config;
 
-import com.vivid.apiserver.domain.user.domain.Role;
-import com.vivid.apiserver.global.auth.JwtAuthExceptionFilter;
-import com.vivid.apiserver.global.auth.JwtAuthFilter;
-import com.vivid.apiserver.global.auth.RestAuthenticationEntryPoint;
-import com.vivid.apiserver.global.auth.application.OAuthSuccessHandler;
-import com.vivid.apiserver.global.auth.application.TokenProvider;
+
 import java.util.List;
+
+import com.server.bbo_gak.global.auth.JwtAuthExceptionFilter;
+import com.server.bbo_gak.global.auth.JwtAuthFilter;
+import com.server.bbo_gak.global.auth.RestAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -34,6 +33,7 @@ public class SecurityConfig {
 
     private final TokenProvider tokenProvider;
 
+    String[] allowUrls = {"/", "/swagger-ui/**", "/v3/**", "/docs.html", "/api-docs/**", "/member/login", "/member/teacher/signUp", "/member/student/signUp"};
 
     @Bean
     public WebSecurityCustomizer configure() {
@@ -62,40 +62,27 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         // cors 관련 설정
-        http.cors().configurationSource(configurationSource());
 
-        http.httpBasic().disable()
-                .csrf().disable()
-                .formLogin().disable() // 로그인 폼 미사용
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .headers().frameOptions().disable()
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(new RestAuthenticationEntryPoint()) // 인증,인가가 되지 않은 요청 시 발생시
-                .and()
-                .authorizeRequests()
-                .antMatchers("/"
-                        , "/css/**"
-                        , "/images/**"
-                        , "/js/**"
-                        , "/h2-console/**"
-                ).permitAll()
-                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll() // 추가
-                .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**").permitAll()
-                .antMatchers("/api/login/**", "/api/auth/**", "/success-login").permitAll() // Security 허용 url
-                .antMatchers("/api/**").hasRole(Role.USER.name())   // 모든 api 요청에 대해 user 권한
-                .anyRequest().authenticated()   // 나머지 요청에 대해서 권한이 있어야함
-                .and()
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .cors(customizer -> customizer.configurationSource(configurationSource()))
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(config -> config
+                        .authenticationEntryPoint(new RestAuthenticationEntryPoint()))// 인증,인가가 되지 않은 요청 시 발생시
+                .authorizeHttpRequests(request -> request
+                    .requestMatchers(allowUrls).permitAll()
+                    .anyRequest().authenticated())
+                .addFilterBefore(new JwtAuthFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtAuthExceptionFilter(), JwtAuthFilter.class);
+
+        http
                 .oauth2Login()
                 .successHandler(oAuthSuccessHandler)
                 .userInfoEndpoint().userService(auth2UserService)
-                .and()
                 .authorizationEndpoint().baseUri("/login"); // 소셜 로그인 url
 
-        http
-                .addFilterBefore(new JwtAuthFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new JwtAuthExceptionFilter(), JwtAuthFilter.class);
 
         return http.build();
     }
