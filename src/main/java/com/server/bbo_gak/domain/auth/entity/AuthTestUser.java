5cc0abe7157c460dd46d4984b43674ec723b2ee0
lee-june-young <uhiccup@gmail.com>
2024-08-03 20:46:48 +0900

    [fix][#2] AuthTestUser 삭제 후 복구

--- src/main/java/com/server/bbo_gak/domain/auth/entity/AuthTestUser.java
+++ src/main/java/com/server/bbo_gak/domain/auth/entity/AuthTestUser.java
@@ -1,7 +1,6 @@
 package com.server.bbo_gak.domain.auth.entity;
 
 import com.server.bbo_gak.domain.user.entity.User;
-import com.server.bbo_gak.domain.user.entity.UserRole;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
@@ -19,9 +18,4 @@ public class AuthTestUser extends User {
 
     private String password;
 
-    public AuthTestUser(String name, String email, UserRole role, String loginId, String password) {
-        super(name, email, role);
-        this.loginId = loginId;
-        this.password = password;
-    }
 }
