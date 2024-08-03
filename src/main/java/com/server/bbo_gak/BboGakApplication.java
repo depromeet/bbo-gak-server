4f79d64f44f519be1953b5f1bbaef34e43453e41
Byuk_mm <jsb100800@naver.com>
2024-08-03 20:50:15 +0900

    [chore][#23] ingress controller swagger 설정
    [chore][#23] 스웨거 셋팅 테스트
    
    [chore][#23] 스웨거 셋팅 테스트

--- src/main/java/com/server/bbo_gak/BboGakApplication.java
+++ src/main/java/com/server/bbo_gak/BboGakApplication.java
@@ -1,11 +1,18 @@
 package com.server.bbo_gak;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "/")
+    }
+)
 public class BboGakApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/server/bbo_gak/global/config/security/SecurityConfig.java
+++ src/main/java/com/server/bbo_gak/global/config/security/SecurityConfig.java
@@ -27,7 +27,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfig {
 
     private final JwtTokenService jwtTokenService;
-    private String[] allowUrls = {"/", "/api/v1/users/test/login", "/docs/**", "/v3/**", "/favicon.ico"};
+    private String[] allowUrls = {"/", "/api/v1/users/test/login", "/docs/**", "/v3/**", "/favicon.ico",
+        "/api/docs/**", "/api/v3/**"};
 
     @Bean
     public WebSecurityCustomizer configure() {
