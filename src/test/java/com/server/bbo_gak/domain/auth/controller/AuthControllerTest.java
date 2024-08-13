5cdcce684df2de3ec6748e67c4a6a4ff21c8556f
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-08-13 21:29:59 +0900

    [feat][#28]통합 테스트 완료

--- src/test/java/com/server/bbo_gak/domain/auth/controller/AuthControllerTest.java
+++ src/test/java/com/server/bbo_gak/domain/auth/controller/AuthControllerTest.java
@@ -1,42 +1,67 @@
 package com.server.bbo_gak.domain.auth.controller;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static com.server.bbo_gak.global.security.jwt.service.JwtTokenService.TOKEN_ROLE_NAME;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.server.bbo_gak.domain.auth.dto.request.LoginRequest;
 import com.server.bbo_gak.domain.auth.dto.request.RefreshTokenRequest;
-import com.server.bbo_gak.domain.auth.service.AuthService;
-import com.server.bbo_gak.domain.user.entity.User;
+import com.server.bbo_gak.domain.user.entity.UserRole;
 import com.server.bbo_gak.global.AbstractRestDocsTests;
 import com.server.bbo_gak.global.RestDocsFactory;
-import com.server.bbo_gak.global.error.exception.BusinessException;
-import com.server.bbo_gak.global.error.exception.ErrorCode;
-import com.server.bbo_gak.global.error.exception.NotFoundException;
 import com.server.bbo_gak.global.security.jwt.dto.TokenDto;
-import io.jsonwebtoken.ExpiredJwtException;
+import com.server.bbo_gak.global.security.jwt.entity.RefreshTokenRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpMethod;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@Sql("/auth-test-data.sql")
 public class AuthControllerTest extends AbstractRestDocsTests {
 
     private static final String DEFAULT_URL = "/api/v1/users";
-    @MockBean
-    private AuthService authService;
+
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
 
     @Autowired
     private RestDocsFactory restDocsFactory;
+
+    @Value("${jwt.refresh-token-secret}")
+    private String jwtRTSecret;
+
+    @Value("${jwt.refresh-token-expiration-time}")
+    private Long RTexpiration;
+
+    @Value("${jwt.issuer}")
+    private String issuer;
+
+    private String refreshToken;
+
+    @BeforeEach
+    void setUp() {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + RTexpiration);
+        refreshToken = Jwts.builder()
+            .setSubject("1")
+            .setIssuedAt(now)
+            .setExpiration(validity)
+            .claim(TOKEN_ROLE_NAME, UserRole.USER.getValue())
+            .signWith(Keys.hmacShaKeyFor(jwtRTSecret.getBytes()), SignatureAlgorithm.HS256)
+            .setIssuer(issuer)
+            .compact();
+    }
 
     @Nested
     class 로그인 {
@@ -49,14 +74,14 @@ public class AuthControllerTest extends AbstractRestDocsTests {
                 .build();
 
             LoginRequest loginRequest = new LoginRequest("test", "test123");
-
-            when(authService.login(any(LoginRequest.class))).thenReturn(tokenDto);
-
             //then
             mockMvc.perform(restDocsFactory.createRequest(DEFAULT_URL + "/test/login", loginRequest, HttpMethod.POST,
                     objectMapper))
                 .andExpect(status().isOk())
                 .andDo(restDocsFactory.getSuccessResource("[로그인] 성공", "jwt 토큰 생성", "auth", loginRequest, tokenDto));
+
+            assertTrue(refreshTokenRepository.findById(1L).isPresent());
+
         }
 
         @Test
@@ -64,8 +89,6 @@ public class AuthControllerTest extends AbstractRestDocsTests {
 
             // Invalid request (missing loginId)
             LoginRequest invalidLoginRequest = new LoginRequest("test", "wrong");
-            when(authService.login(any(LoginRequest.class))).thenThrow(
-                new BusinessException(ErrorCode.PASSWORD_NOT_MATCHES));
 
             mockMvc.perform(
                     restDocsFactory.createRequest(DEFAULT_URL + "/test/login", invalidLoginRequest, HttpMethod.POST,
@@ -79,8 +102,6 @@ public class AuthControllerTest extends AbstractRestDocsTests {
 
             // Invalid request (missing loginId)
             LoginRequest invalidLoginRequest = new LoginRequest("wrong", "test123");
-            when(authService.login(any(LoginRequest.class))).thenThrow(
-                new NotFoundException(ErrorCode.USER_NOT_FOUND));
 
             mockMvc.perform(
                     restDocsFactory.createRequest(DEFAULT_URL + "/test/login", invalidLoginRequest, HttpMethod.POST,
@@ -102,11 +123,7 @@ public class AuthControllerTest extends AbstractRestDocsTests {
                 .refreshToken("refreshToken")
                 .build();
 
-            String validRefreshToken = "validRefreshToken";
-
-            RefreshTokenRequest request = new RefreshTokenRequest(validRefreshToken);
-
-            when(authService.validateRefreshToken(any(RefreshTokenRequest.class))).thenReturn(tokenDto);
+            RefreshTokenRequest request = new RefreshTokenRequest(refreshToken);
 
             //then
             mockMvc.perform(restDocsFactory.createRequest(DEFAULT_URL + "/refreshToken", request, HttpMethod.POST,
@@ -128,8 +145,6 @@ public class AuthControllerTest extends AbstractRestDocsTests {
 
             RefreshTokenRequest request = new RefreshTokenRequest(validRefreshToken);
 
-            when(authService.validateRefreshToken(any(RefreshTokenRequest.class))).thenThrow(
-                new ExpiredJwtException(null, null, "Expired"));
             //then
             mockMvc.perform(restDocsFactory.createRequest(DEFAULT_URL + "/refreshToken", request, HttpMethod.POST,
                     objectMapper))
@@ -145,13 +160,14 @@ public class AuthControllerTest extends AbstractRestDocsTests {
         @Test
         public void 성공() throws Exception {
 
-            doNothing().when(authService).logout(any(User.class));
-
             //then
             mockMvc.perform(
                     restDocsFactory.createRequest(DEFAULT_URL + "/logout", null, HttpMethod.GET, objectMapper))
                 .andExpect(status().isOk())
                 .andDo(restDocsFactory.getSuccessResource("[로그아웃] 성공", "로그아웃", "auth", null, null));
+
+            assertTrue(refreshTokenRepository.findById(1L).isEmpty());
+
         }
     }
 }
