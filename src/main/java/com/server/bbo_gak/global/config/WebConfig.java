6c25cb04a877e916277d423651eac2f64b48cdf6
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-08-22 19:20:21 +0900

    [hotfix] swagger 안됨 에러 중지

--- src/main/java/com/server/bbo_gak/global/config/WebConfig.java
+++ src/main/java/com/server/bbo_gak/global/config/WebConfig.java
@@ -32,5 +32,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authenticationArgumentResolver);
+
     }
 }
