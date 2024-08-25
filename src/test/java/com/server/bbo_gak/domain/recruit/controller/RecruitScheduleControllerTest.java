12d9ead0b904ec3995ea958f56bffb9bdbfee6e2
lee-june-young <uhiccup@gmail.com>
2024-08-25 18:27:05 +0900

    [feat][#56] 공고 일정 - 공고단계 & 마감일 수정 ControllerTest 생성

--- src/test/java/com/server/bbo_gak/domain/recruit/controller/RecruitScheduleControllerTest.java
+++ src/test/java/com/server/bbo_gak/domain/recruit/controller/RecruitScheduleControllerTest.java
@@ -3,6 +3,8 @@ package com.server.bbo_gak.domain.recruit.controller;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.server.bbo_gak.domain.recruit.dto.request.RecruitScheduleCreateRequest;
+import com.server.bbo_gak.domain.recruit.dto.request.RecruitScheduleUpdateDeadLineRequest;
+import com.server.bbo_gak.domain.recruit.dto.request.RecruitScheduleUpdateStageRequest;
 import com.server.bbo_gak.domain.recruit.dto.response.RecruitScheduleGetResponse;
 import com.server.bbo_gak.domain.recruit.entity.RecruitScheduleStage;
 import com.server.bbo_gak.global.AbstractRestDocsTests;
@@ -20,7 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @SpringBootTest
 @ActiveProfiles("test")
-@Sql({"/all-data-delete.sql", "/recruit-test-data.sql"})
+@Sql({"/all-data-delete.sql", "/recruit-schedule-test-data.sql"})
 class RecruitScheduleControllerTest extends AbstractRestDocsTests {
 
   private static final String DEFAULT_URL = "/api/v1/recruits/{id}/recruit-schedule";
@@ -70,6 +72,36 @@ class RecruitScheduleControllerTest extends AbstractRestDocsTests {
           .andDo(restDocsFactory.getSuccessResourceList("[GET] 공고일정 전체 조회 성공", "공고일정 전체 조회 ", "RecruitSchedule", List.of(),
               List.of(response1, response2)));
     }
-
   }
+
+  @Nested
+  class 공고일정_공고단계_업데이트 {
+    @Test
+    public void 성공() throws Exception {
+      RecruitScheduleUpdateStageRequest request = new RecruitScheduleUpdateStageRequest(
+          "2차 면접"
+      );
+
+      mockMvc.perform(
+              restDocsFactory.createRequest(DEFAULT_URL+"/{recruit-schedule-id}/stage", request, HttpMethod.PUT, objectMapper, 1L, 1L))
+          .andExpect(status().isOk())
+          .andDo(restDocsFactory.getSuccessResource("[PUT] 공고단계 업데이트 성공", "공고단계 업데이트", "RecruitSchedule", request, null));
+    }
+  }
+
+  @Nested
+  class 공고일정_마감일_업데이트 {
+    @Test
+    public void 성공() throws Exception {
+      RecruitScheduleUpdateDeadLineRequest request = new RecruitScheduleUpdateDeadLineRequest(
+          "2024-12-25"
+      );
+
+      mockMvc.perform(
+              restDocsFactory.createRequest(DEFAULT_URL+"/{recruit-schedule-id}/deadLine", request, HttpMethod.PUT, objectMapper, 1L, 1L))
+          .andExpect(status().isOk())
+          .andDo(restDocsFactory.getSuccessResource("[PUT] 공고마감일 업데이트 성공", "공고마감일 업데이트", "RecruitSchedule", request, null));
+    }
+  }
+
 }
---
+++ src/test/resources/recruit-schedule-test-data.sql
@@ -0,0 +1,59 @@
+INSERT INTO users (deleted, created_at, update_at, user_id, dtype, email, login_id, name, password, role)
+VALUES (false, '2024-07-24 21:27:20.000000', '2024-07-24 21:27:21.000000', 1, 'AuthTestUser', 'test', 'test', 'test',
+        'test', 'USER'),
+       (false, '2024-07-24 21:27:20.000000', '2024-07-24 21:27:21.000000', 2, 'AuthTestUser', 'test', 'test', 'test',
+        'test', 'USER'),
+       (false, '2024-07-24 21:27:20.000000', '2024-07-24 21:27:21.000000', 3, 'AuthTestUser', 'test', 'test', 'test',
+        'test', 'USER')
+;
+
+-- Season 테이블에 데이터 삽입
+INSERT INTO recruit_season (recruit_season_id, name, user_id, deleted, update_at, created_at)
+VALUES (1, '2024 상반기', 1, false, '2024-07-24 21:27:20.000000', '2024-07-24 21:27:21.000000'),
+       (2, '2024 하반기', 1, false, '2024-07-24 21:27:20.000000', '2024-07-24 21:27:21.000000'),
+       (3, '2024 상반기', 2, false, '2024-07-24 21:27:20.000000', '2024-07-24 21:27:21.000000'),
+       (4, '2024 하반기', 2, false, '2024-07-24 21:27:20.000000', '2024-07-24 21:27:21.000000'),
+       (5, '2024 상반기', 3, false, '2024-07-24 21:27:20.000000', '2024-07-24 21:27:21.000000'),
+       (6, '2024 하반기', 3, false, '2024-07-24 21:27:20.000000', '2024-07-24 21:27:21.000000')
+;
+
+-- Recruit 테이블에 데이터 삽입
+INSERT INTO recruit (recruit_id, title, site_url, recruit_status, recruit_season_id, user_id, created_at, update_at,
+                     deleted)
+VALUES (1, 'Title for one day left', 'http://example.com/1', 'DOCUMENT_PASSED', 1, 1, CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP, false),
+       (2, 'Title for more than one day left', 'http://example.com/2', 'PREPARATION_IN_PROGRESS', 2, 1,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
+       (3, 'Title for more than one day left', 'http://example.com/2', 'PREPARATION_IN_PROGRESS', 3, 2,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
+       (4, 'Title for more than one day left', 'http://example.com/2', 'PREPARATION_IN_PROGRESS', 3, 2,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
+       (5, 'Title for more than one day left', 'http://example.com/2', 'PREPARATION_IN_PROGRESS', 4, 2,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
+       (6, 'Title for more than one day left', 'http://example.com/2', 'PREPARATION_IN_PROGRESS', 5, 3,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false),
+       (7, 'Title for more than one day left', 'http://example.com/2', 'PREPARATION_IN_PROGRESS', 6, 3,
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false)
+;
+
+-- RecruitSchedule 테이블에 데이터 삽입
+INSERT INTO recruit_schedule (recruit_schedule_id, recruit_id, recruit_schedule_stage, dead_line, deleted, update_at, created_at)
+VALUES (1, 1, 'FIRST_INTERVIEW', TIMESTAMPADD(DAY, 3, CURRENT_TIMESTAMP), false, '2024-07-24 21:26:28.000000', '2024-07-24 21:26:28.000000'),
+       (2, 4, 'FIRST_INTERVIEW', TIMESTAMPADD(DAY, 3, CURRENT_TIMESTAMP), false, '2024-07-24 21:26:28.000000', '2024-07-24 21:26:28.000000'),
+       (3, 5, 'FIRST_INTERVIEW', TIMESTAMPADD(DAY, 1, CURRENT_TIMESTAMP), false, '2024-07-24 21:26:28.000000', '2024-07-24 21:26:28.000000'),
+       (4, 6, 'FIRST_INTERVIEW', TIMESTAMPADD(DAY, 2, CURRENT_TIMESTAMP), false, '2024-07-24 21:26:28.000000', '2024-07-24 21:26:28.000000'),
+       (5, 6, 'SECOND_INTERVIEW', TIMESTAMPADD(DAY, 3, CURRENT_TIMESTAMP), false, '2024-07-24 21:26:28.000000', '2024-07-24 21:26:28.000000'),
+       (6, 7, 'FIRST_INTERVIEW', TIMESTAMPADD(DAY, -2, CURRENT_TIMESTAMP), false, '2024-07-24 21:26:28.000000', '2024-07-24 21:26:28.000000'),
+       (7, 7, 'SECOND_INTERVIEW', TIMESTAMPADD(DAY, -1, CURRENT_TIMESTAMP), false, '2024-07-24 21:26:28.000000', '2024-07-24 21:26:28.000000'),
+       (8, 7, 'FINAL_INTERVIEW', TIMESTAMPADD(DAY, 1, CURRENT_TIMESTAMP), false, '2024-07-24 21:26:28.000000', '2024-07-24 21:26:28.000000')
+;
+
+INSERT INTO card (deleted, copy_flag, access_time, card_id, created_at, update_at, user_id, content, title, recruit_id)
+VALUES (false, false, '2024-07-24 21:22:04.000000', 1, '2024-07-24 21:22:07.000000', '2024-07-24 21:22:08.000000', 1,
+        'test_contents', 'test_title', 1);
+INSERT INTO card (deleted, copy_flag, access_time, card_id, created_at, update_at, user_id, content, title, recruit_id)
+VALUES (false, false, '1970-01-01 00:00:00.001000', 2, '1970-01-01 00:00:00.001000', '1970-01-01 00:00:00.001000', 1,
+        'testc', 'testc', 1);
+INSERT INTO card (deleted, copy_flag, access_time, card_id, created_at, update_at, user_id, content, title, recruit_id)
+VALUES (false, false, '1970-01-01 00:00:00.001000', 3, '1970-01-01 00:00:00.001000', '1970-01-01 00:00:00.001000', 1,
+        'testc', 'testt', 1);
